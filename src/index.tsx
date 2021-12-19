079000a98f610e90ddf5984f65f53e23c6f289bd
nbogie <neillbogie@googlemail.com>
2021-12-19 16:14:38 +0000

    add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TownshipVoiceInterface from './components/TownshipVoiceInterface';
 
+//fonts
+import "@fontsource/abel"
+import "@fontsource/merriweather"
+
+
 ReactDOM.render(
   <React.StrictMode>
     <TownshipVoiceInterface />
